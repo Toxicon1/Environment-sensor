15695923d3ffd8c5f5dcb9f9947820f93998cc26
Toxicon <tomas.cuyx@gmail.com>
2020-08-17 23:12:14 +0200

    commit

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+#include "DHT 22"
+#include "liquidCrystal_I2C"
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
